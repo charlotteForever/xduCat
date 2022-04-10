258b1babf1730e5e4d376112ea3b297c2c2914ca
姜畅 <jc2371372895@163.com>
2022-04-10 04:30:35 +0000

    update xducat-client/src/views/LoginView.vue. 美化了登陆页面

--- xducat-client/src/views/LoginView.vue
+++ xducat-client/src/views/LoginView.vue
@@ -1,27 +1,39 @@
 <template>
+
+  <div class="login_container">
+    <!--外框-->
+    <div class="login_box">
+      <!-- 头像 -->
+  <div class="logo">
+    <img src="../../img/catlogo.png" alt="">
+  </div>
+      <!-- 表单 -->
+      <el-form ref="formRef"
+               :model="FormData"
+               label-width="0px"
+               class="sign"
+               :rules="Login">
   <el-form ref="loginRef" :model="login" :rules="rules">
-
+    <!--登录名-->
     <el-form-item prop="username">
-      <el-input v-model="login.username" placeholder="用户名" >
-        <template #prefix>
-          <el-icon class="el-input__icon" id="username" :size="20"><user/></el-icon>
-        </template>
-      </el-input>
+      <el-input v-model="login.username" placeholder="用户名" prefix-icon="iconfont icon-user" ></el-input>
     </el-form-item>
-
+      <!--密码-->
     <el-form-item prop="password">
-      <el-input v-model="login.password" type="password" placeholder="密码" show-password >
-        <template #prefix>
-          <el-icon class="el-input__icon" id="password" :size="20"><lock /></el-icon>
-        </template>
-      </el-input>
+      <el-input v-model="login.password" type="password" placeholder="密码" show-password
+                prefix-icon="iconfont icon-3702mima"></el-input>
     </el-form-item>
 
+    <!--按钮-->
     <el-form-item>
-      <el-button type="primary" @click="onLogin('loginRef')">登录</el-button>
+      <el-button type="primary" @click="onLogin('loginRef')" >登录</el-button>
     </el-form-item>
   </el-form>
+      </el-form>
+    </div>
+  </div>
 </template>
+
 
 <script lang="ts" setup>
 import { reactive } from 'vue'
@@ -98,4 +110,58 @@ export default defineComponent ({
   }
 })
 </script>
+<style lang="less" scoped>
+
+.login_container {
+  height: 100%;
+  background-image: url("../../img/catbackground.jpg");
+  background-size: 100% 100%;
+  width: 100%;
+  position: fixed;
+}
+
+.login_box {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 300px;
+  width: 450px;
+  border-radius: 14px;
+  background-color: #dcdddd;
+  .logo {
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 130px;
+    height: 130px;
+    border-radius: 50%;
+    border: 1px solid #eee;
+    padding: 10px;
+    background-color: #fff;
+    box-shadow: 0 0 10px #ddd;
+    img {
+      height: 100%;
+      width: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+  .el-button--primary
+  {
+    color: #ffffff;
+    background:#00C07A;
+    border-color: #00C07A;
+  }
+  .sign {
+    margin-top: 90px;
+    padding: 0 20px;
+
+    .btns {
+      display: flex;
+      justify-content: flex-end;
+    }
+  }
+}
+</style>
 
